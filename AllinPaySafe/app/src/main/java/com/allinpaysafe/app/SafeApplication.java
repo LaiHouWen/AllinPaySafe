e2c4a2c97f55389cbc34a3025bd7b06c57ff6c44
LaiHouWen <1028338041@qq.com>
2017-08-31 14:42:07 +0800

    add allinpaysafe

---
+++ AllinPaySafe/app/src/main/java/com/allinpaysafe/app/SafeApplication.java
@@ -0,0 +1,15 @@
+package com.allinpaysafe.app;
+
+import android.app.Application;
+
+/**
+ * Created by houwen.lai on 2017/8/31.
+ *
+ */
+
+public class SafeApplication extends Application {
+
+
+
+
+}
